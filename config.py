2cd89d899ac8a6021eff58853d4505d1c8f62b80
Sarthak1970 <katiyarsarthak8@gmail.com>
2026-05-01 22:20:37 +0530

    training

---
+++ config.py
@@ -0,0 +1,30 @@
+def get_config():
+    return {
+        "batch_size":2,
+        "num_epochs":10,
+        "lr":10**-4,
+        "seq_len":128,
+        "datasource": 'iitb',
+        "lang_src":"en",
+        "lang_tgt":"hi",
+        "model_folder":"weights",
+        "model_filename":"tmodel",
+        "preload":None,
+        "tokenizer_file":"tokenizer_{0}.json",
+        "experiment_name": "runs/tmodel",
+        'd_model': 512,
+    }
+
+def get_weights_file_path(config, epoch: str):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_filename']}{epoch}.pt"
+    return str(Path('.') / model_folder / model_filename)
+
+def latest_weights_file_path(config):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_filename']}*"
+    weights_files = list(Path(model_folder).glob(model_filename))
+    if len(weights_files) == 0:
+        return None
+    weights_files.sort()
+    return str(weights_files[-1])
---
+++ hfupload.py
@@ -0,0 +1,53 @@
+import torch
+from pathlib import Path
+from huggingface_hub import HfApi, upload_file
+import json
+
+repo_id = "Sarthak1970/transformer-en-hi"
+model_folder = Path("iitb_weights") 
+config = {
+    "seq_len": 128,
+    "d_model": 512,
+    "lang_src": "en",
+    "lang_tgt": "hi"
+}
+
+
+api = HfApi()
+api.create_repo(repo_id=repo_id, exist_ok=True)
+
+weight_files = sorted(model_folder.glob("*.pt"))
+if not weight_files:
+    raise Exception("No model weights found!")
+
+latest_model_path = weight_files[-1]
+print(f"Using model: {latest_model_path}")
+
+state = torch.load(latest_model_path, map_location="cpu")
+
+final_model_path = "pytorch_model.pt"
+torch.save(state["model_state_dict"], final_model_path)
+
+with open("config.json", "w") as f:
+    json.dump(config, f, indent=4)
+
+files_to_upload = [
+    final_model_path,
+    "config.json",
+    "tokenizer_en.json",
+    "tokenizer_hi.json",
+]
+
+for file in files_to_upload:
+    path = Path(file)
+    if path.exists():
+        upload_file(
+            path_or_fileobj=str(path),
+            path_in_repo=path.name,
+            repo_id=repo_id,
+        )
+        print(f"Uploaded: {file}")
+    else:
+        print(f"Skipped (not found): {file}")
+
+print("Your model is pushed to hf successfully ")
